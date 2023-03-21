24e7acb50c118d8ecb454d03d4b2e8298d2a71ad
sisusisoo <khc97622@naver.com>
2023-03-21 12:37:16 +0900

    Initial Commit

---
+++ poly_1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  poly_1
+//
+//  Created by 강현철 on 2023/03/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
